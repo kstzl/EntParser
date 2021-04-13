f39bbb948489f3396b0866328c779555ec62e492
Kevin <kevin.stoetzel@gmail.com>
2021-04-13 21:15:52 +0200

    fixed a lot of things

--- ent_parser.py
+++ ent_parser.py
@@ -1,51 +1,70 @@
 
 File = """
-PointLightA;
+PointLight;
 {
-Color:0,0,255;
+Position:-48,48,-64;
+Color:255,255,255;
 Range:500;
 };
-PointLightB;
+PointLight;
 {
-Color:0,255,0;
-Dist:300;
-Blabla:hello;
+Position:48,48,-64;
+Color:255,255,255;
+Range:500;
+};
+PointLight;
+{
+Position:-48,48,48;
+Color:255,255,255;
+Range:500;
+};
+PointLight;
+{
+Position:64,48,48;
+Color:255,255,255;
+Range:500;
 };
 """
 
 def get_entities(file_):
     mode = "type"
     actual_ent_name = ""
-    ents = {}
+    ent = {}
+    ents = []
+
     for e in file_.split("\n"):
         if mode == "type" and e != "":
             actual_ent_name = e
-            ents[ actual_ent_name ] = []
+            if actual_ent_name.endswith(";"):
+                actual_ent_name = actual_ent_name[:-1]
+            ent[ actual_ent_name ] = []
             mode = "inside"
         elif mode == "inside":
             if e != "{" and e != "};":
-                ents[ actual_ent_name ].append(e)
+                ent[ actual_ent_name ].append(e)
             if e == "};":
+                ents.append({"name" : actual_ent_name, "content" : ent[ actual_ent_name ] } )
                 mode = "type"
     return ents
 
 def get_arg(arg_):
-    arg_splitted = arg_.split(":")
-    arg_name = arg_splitted[0]
-    arg_content = arg_splitted[1]
-    if arg_content.endswith(";"):
-        arg_content = arg_content[:-1]
-    arg_content_splitted = arg_content.split(",")
+    arg_content = arg_["content"]
+    ret = {}
+    rets = []
+    for e in arg_content:
+        arg_s = e.split(":")
+        arg_name = arg_s[0]
+        arg_content = arg_s[1]
+        ret[arg_name] = []
+        for c in arg_content.split(","):
+            if c.endswith(";"):
+                c = c[:-1]
+            ret[arg_name].append(c)
 
-    return {"name" : arg_name, "content" : arg_content_splitted}
-
-
+        return ret
 
 if __name__ == "__main__":
     Ents = get_entities(File)
-
     for e in Ents:
-        print(f"---> {e}")
-        for a in Ents[e]:
-            b = get_arg(a)
-            print(f"{ b['name'] } ---> { b['content'] }")
+        print(f"------> {e['name']}")
+        print(f"---> {get_arg(e)['Position']}")
